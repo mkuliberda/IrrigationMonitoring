13d1d893fa71e1508193cb43cf107916c9321d1c
Mateusz <mkuliberda@gmail.com>
2020-03-16 20:05:21 +0100

    Scheduler importedd as module with example usage

--- monitor.py
+++ monitor.py
@@ -1,6 +1,7 @@
-import nrf24l01_drv as nrf
+import nrf24l01_drv as nrf_radio
 import time
-#import nrf24l01_test
+import Scheduler
+
 
 RADIO1_CE_PIN = 26   # BCM pins numbering
 RADIO1_IRQ_PIN = 13  # BCM pins numbering 
@@ -11,18 +12,27 @@ RADIO1_CHANNEL = 15
 RADIO1_MY_ADDRESS = [231, 231, 231, 231, 231]  # 0xe7 is 231
 RADIO1_TX_ADDRESS = [126, 126, 126, 126, 126]  # 0x7e is 126
 
+PLANTS_SCHEDULES = ["plants_group1_schedule.xml", "plants_group2_schedule.xml", "plants_group3_schedule.xml"]
 
 
-radio1 = nrf.NRF24L01()
+plants_schedule = Scheduler.Scheduler(PLANTS_SCHEDULES)
+print(plants_schedule.is_active_by_tag("Pelargonia"))
+print(plants_schedule.is_active_by_tag("Surfinia"))
+print(plants_schedule.is_active_all())
+print(plants_schedule.print_full_schedule())
+
+
+
+radio1 = nrf_radio.NRF24L01()
 radio1.init(RADIO1_SPIDEV, RADIO1_SPICS, RADIO1_CE_PIN, RADIO1_IRQ_PIN)
-config = radio1.config(RADIO1_PAYLOAD_SIZE, RADIO1_CHANNEL, nrf.NRF24L01_OutputPower.NRF24L01_OutputPower_0dBm, nrf.NRF24L01_DataRate.NRF24L01_DataRate_2M)
+config = radio1.config(RADIO1_PAYLOAD_SIZE, RADIO1_CHANNEL, nrf_radio.NRF24L01_OutputPower.NRF24L01_OutputPower_0dBm, nrf_radio.NRF24L01_DataRate.NRF24L01_DataRate_2M)
 radio1.set_my_address(RADIO1_MY_ADDRESS)
 radio1.set_tx_address(RADIO1_TX_ADDRESS)
 
 tx_payload = [218] * RADIO1_PAYLOAD_SIZE
 radio1.transmit_payload(tx_payload)
 time.sleep(0.2)
-print(nrf.NRF24L01_TransmitStatus.NRF24L01_Transmit_Status_Sending, radio1.get_transmission_status())
+print(nrf_radio.NRF24L01_TransmitStatus.NRF24L01_Transmit_Status_Sending, radio1.get_transmission_status())
 radio1.power_up_rx()
 
 print(radio1.get_status())
