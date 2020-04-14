be0227b57eac4331791201d4a68cf65df282f670
Mateusz <mkuliberda@gmail.com>
2020-04-14 19:53:28 +0200

    confirmations handling further work

--- monitor.py
+++ monitor.py
@@ -14,11 +14,12 @@ RADIO1_PAYLOAD_SIZE = 32
 RADIO1_CHANNEL = 15
 RADIO1_MY_ADDRESS = [231, 231, 231, 231, 231]  # 0xe7 is 231
 RADIO1_TX_ADDRESS = [126, 126, 126, 126, 126]  # 0x7e is 126
-messages_to_send_queue = []
-messages_received_queue = []
-
+COMMS_REFRESH_RATE_MS = 50
 
 PLANTS_SCHEDULES = ["plants_group1_schedule.xml", "plants_group2_schedule.xml", "plants_group3_schedule.xml"]
+SCHEDULE_REFRESH_RATE_MS = 1000
+
+MONITOR_REFRESH_RATE_MS = 1000
 
 lock = threading.RLock()
 
@@ -29,7 +30,7 @@ irrigation_time_event = threading.Event()
 
 
 class schedulingThread(threading.Thread):
-        def __init__(self, schedules):
+        def __init__(self, schedules, refresh_rate_ms=1000):
                 self._schedule = Scheduler.Scheduler(schedules)
                 self._tags = self._schedule.get_tags()
                 self._activity_previous = dict.fromkeys(self._tags, False)
@@ -37,6 +38,7 @@ class schedulingThread(threading.Thread):
                 self._activity_queue = []
                 self._running = True
                 self._activity_event = threading.Event()
+                self._refresh_rate_s = refresh_rate_ms/1000.0
                 threading.Thread.__init__(self)
 
         def terminate(self):
@@ -48,9 +50,9 @@ class schedulingThread(threading.Thread):
         def run(self):
                 while self._running:
                         if self._activity_changed() is True:
-                                print("send_event")
+                                print("irrigation activity change")
                                 self._activity_event.set()
-                        time.sleep(1)
+                        time.sleep(self._refresh_rate_s)
         
         def _activity_changed(self):
                 event_flag = False
@@ -76,14 +78,15 @@ class schedulingThread(threading.Thread):
 
 
 class communicationsThread(threading.Thread):
-        def __init__(self, spi_dev, spi_cs, ce_pin, irq_pin, payload_size, channel, out_pwr, datarate, src_addr, dest_addr):
+        def __init__(self, spi_dev, spi_cs, ce_pin, irq_pin, payload_size, channel, out_pwr, datarate, src_addr, dest_addr, refresh_rate_ms=20):
                 self._radio1 = wireless.NRF24L01()
                 self._radio1.init(spi_dev, spi_cs, ce_pin, irq_pin)
                 self._radio1_configured = self._radio1.config(payload_size, channel, out_pwr, datarate)
                 self._outbound_msg_queue = collections.deque() 
                 self._inbound_msg_queue = collections.deque()
-                self._confirmation_msg_queue = collections.deque()
-                self._awaiting_confirmation_queue = collections.deque()
+                self._awaiting_confirmation_msg_queue = collections.deque()
+                self._refresh_rate_s = refresh_rate_ms/1000.0
+                self._counter = 0
                 if self._radio1_configured is True: 
                         self._radio1.set_my_address(src_addr)
                         self._radio1.set_tx_address(dest_addr)
@@ -92,6 +95,7 @@ class communicationsThread(threading.Thread):
                         self._running = False
                         print("configuration of radio1 failed, terminating thread...")
                 self._comm_lost_event = threading.Event()
+                self._new_message_event = threading.Event()               
                 threading.Thread.__init__(self)
 
         def terminate(self):
@@ -104,57 +108,81 @@ class communicationsThread(threading.Thread):
                 while self._running:
                         while self._radio1.data_ready() == 1:
                                 payload = self._radio1.get_payload()
-                                print("payload ", payload)
+                                #print("payload ", payload)
                                 if payload[0] == wireless.direction_t.from_irm_to_rpi:  #standard message
                                         msg = wireless.IrrigationMessage(wireless.direction_t.from_irm_to_rpi)
                                         msg.set_buffer(payload, RADIO1_PAYLOAD_SIZE)
                                         if msg.validate_crc():
-                                                print("validated msg received ", msg.decode_message())
+                                                print("valid msg received:", msg.decode_message())
                                         else:
-                                                print("crc didn't pass, todo...")
+                                                print("crc error")
                                         del msg
 
                                 elif payload[0] == wireless.direction_t.from_rpi_to_irm:  #confirmation message
                                         msg = wireless.IrrigationMessage(wireless.direction_t.from_irm_to_rpi)
                                         msg.set_buffer(payload, RADIO1_PAYLOAD_SIZE)
                                         if msg.validate_crc():
-                                                #self._confirmation_msg_queue.append(payload)
-                                                print("confirmation ", msg.decode_confirmation())
+                                                confirmation_dict = msg.decode_confirmation()
+                                                if confirmation_dict['consumed'] == True:
+                                                        del confirmation_dict['consumed']
+                                                        for aw_confirmation_dict in list(self._awaiting_confirmation_msg_queue):
+                                                                if aw_confirmation_dict == confirmation_dict:
+                                                                        print("command: ",confirmation_dict['cmd']," confirmed")
+                                                                        self._awaiting_confirmation_msg_queue.remove(aw_confirmation_dict)
                                         else:
-                                                print("crc didn't pass, todo...")
+                                                print("crc error")
                                         del msg
                                 else:
                                         print("unknown message")
-
+                        
                         self._send_avbl_messages()
+                        time.sleep(self._refresh_rate_s)
 
-                        time.sleep(0.05)  #20Hz
-
-        def configure_notification_event(self, event):
+        def configure_comm_lost_event(self, event):
                 self._comm_lost_event = event
+
+        def configure_new_message_event(self, event):
+                self._new_message_event = event
+        
+        def get_awaiting_confirmation_msg_queue(self):
+                return self._awaiting_confirmation_msg_queue
+
+        def _convert_cmd_to_dict(self, _cmd):
+                return {'target': _cmd[1], 'target_id': _cmd[2], 'cmd': _cmd[3], 'subcmd1': 0, 'subcmd2': 0}
 
         def add_msg_to_queue(self, payload):
                 self._outbound_msg_queue.append(payload)
+
+        def _command_retry(self):  #WIP
+                while len(self._awaiting_confirmation_msg_queue) > 0:
+                        msg_dict = self._awaiting_confirmation_msg_queue.pop()
+                        outbound_msg = wireless.IrrigationMessage(wireless.direction_t.from_rpi_to_irm)
+                        irrigation_cmd = wireless.cmd_s()
+                        irrigation_cmd.target = wireless.target_t(msg_dict['target'])
+                        irrigation_cmd.target_id = msg_dict['target_id']
+                        irrigation_cmd.cmd = wireless.command_t(msg_dict['cmd'])
+                        payload = outbound_msg.encode_cmd(irrigation_cmd)
+                        self._outbound_msg_queue.append(payload)
+                        print("command retry:", payload)
 
         def _send_avbl_messages(self):
                 while len(self._outbound_msg_queue) > 0:
                         msg = self._outbound_msg_queue.pop()
                         if msg[3] == wireless.command_t.Start or msg[3] == wireless.command_t.Stop:  #only Start and Stop commmands are now enabled
-                                self._awaiting_confirmation_queue.append(msg)
-                                print(self._awaiting_confirmation_queue)
+                                self._awaiting_confirmation_msg_queue.append(self._convert_cmd_to_dict(msg))
                         self._radio1.transmit_payload(msg)
                         while self._radio1.get_transmission_status() == wireless.NRF24L01_TransmitStatus.Sending:
                                 pass     
-                        print("message sent")
+                        print("command sent:", self._convert_cmd_to_dict(msg))
                 self._radio1.power_up_rx()
                 while self._radio1.get_status() != 14:
                         time.sleep(0.02)
                         self._radio1.power_up_rx()
         
-        def new_message_count(self):
+        def get_new_message_count(self):
                 return len(self._inbound_msg_queue)
 
-        def retreive_received_messages(self):
+        def retreive_new_message(self):
                 return self._inbound_msg_queue.pop()
 
 def get_status(event):
@@ -165,7 +193,7 @@ def get_status(event):
 
 if __name__ == "__main__":
 
-        irrigation_scheduler = schedulingThread(PLANTS_SCHEDULES)
+        irrigation_scheduler = schedulingThread(PLANTS_SCHEDULES, SCHEDULE_REFRESH_RATE_MS)
         irrigation_scheduler.configure_notification_event(irrigation_time_event)
         irrigation_scheduler.start()
 
@@ -173,8 +201,9 @@ if __name__ == "__main__":
                                                 RADIO1_PAYLOAD_SIZE, RADIO1_CHANNEL,
                                                 wireless.NRF24L01_OutputPower.M6dBm,
                                                 wireless.NRF24L01_DataRate._1Mbps,
-                                                RADIO1_MY_ADDRESS, RADIO1_TX_ADDRESS)
-        wireless_link.configure_notification_event(message_received_event)
+                                                RADIO1_MY_ADDRESS, RADIO1_TX_ADDRESS,
+                                                COMMS_REFRESH_RATE_MS)
+        wireless_link.configure_new_message_event(message_received_event)
         wireless_link.start()
 
         status_checker = threading.Thread(name='status getter', target=get_status, args=(get_status_event,))
@@ -216,7 +245,7 @@ if __name__ == "__main__":
                                 del status_cmd
                         else:
                                 print("there's no new messages")
-                        time.sleep(1)
+                        time.sleep(MONITOR_REFRESH_RATE_MS/1000)
 
                         
         except KeyboardInterrupt:
