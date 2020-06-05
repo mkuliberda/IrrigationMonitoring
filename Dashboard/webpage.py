6040c4a7b70d5d38826c36089e1e9c63da41df47
Mateusz <mkuliberda@gmail.com>
2020-06-05 21:41:48 +0200

    openweathermap added to flask

--- Dashboard/webpage.py
+++ Dashboard/webpage.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import requests
 #from pusher import Pusher
 
 app = Flask(__name__)
@@ -6,6 +7,20 @@ app.config['SESSION_COOKIE_SECURE'] = False
 
 @app.route('/')
 def index():
+    weather_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=63f4d3e36c7caad4496f47edcea1bd23'
+    city = 'Gdansk'
+
+    r = requests.get(weather_url.format(city)).json()
+    print(r)
+
+    weather = {
+        'city': city,
+        'temperature': r['main']['temp'],
+        'real_feel' : r['main']['feels_like'],
+        'description': r['weather'][0]['description'],
+        'icon': r['weather'][0]['icon']
+    }
+    print(weather)
     return render_template('index.html')
 
 if __name__ == '__main__':
