efeb2f770fa7618c91d0755680d916ea08a2de7d
Mateusz <mkuliberda@gmail.com>
2020-06-06 22:10:43 +0200

    weather layout fixed, forecast added

--- Dashboard/webpage.py
+++ Dashboard/webpage.py
@@ -8,20 +8,27 @@ app.config['SESSION_COOKIE_SECURE'] = False
 @app.route('/')
 def index():
     weather_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=63f4d3e36c7caad4496f47edcea1bd23'
+    forecast_url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&appid=63f4d3e36c7caad4496f47edcea1bd23'
     city = 'Gdansk'
 
-    r = requests.get(weather_url.format(city)).json()
-    print(r)
+    curr = requests.get(weather_url.format(city)).json()
+    fore = requests.get(forecast_url.format(city)).json()
+    print(fore)
 
-    weather = {
+    current_weather = {
         'city': city,
-        'temperature': r['main']['temp'],
-        'real_feel' : r['main']['feels_like'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon']
+        'temperature': curr['main']['temp'],
+        'real_feel' : curr['main']['feels_like'],
+        'humidity' : curr['main']['humidity'],
+        'description': curr['weather'][0]['description'],
+        'icon': curr['weather'][0]['icon']
     }
-    print(weather)
-    return render_template('index.html')
+
+    forecast_3hr = {
+
+    }
+    #print(current_weather)
+    return render_template('index.html', current_weather=current_weather)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8082, debug=True)
