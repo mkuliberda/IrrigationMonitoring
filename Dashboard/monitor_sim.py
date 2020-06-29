acc659eac0b2016276adfe833976a9e120fb9142
mkuliberda <m.kuliberda@s4t.com.pl>
2020-06-29 18:49:00 +0200

    Dynamic content on webpage finished. Started work on notifications

--- Dashboard/monitor_sim.py
+++ Dashboard/monitor_sim.py
@@ -36,83 +36,95 @@ sector = [{
 watertank = [{
     'type': 'tank',
     'last_update': '10min',
-    'level': 10,
+    'level': 50,
     'errors':''
     }]
 
 battery = [{
     'type': 'power',
-    'last_update': '10s',
-    'level': 90,
-    'state': 'undetermined',
-    'errors': ''
+    'last_update': '14s',
+    'level': 91,
+    'state': 'charging',
+    'errors': 'overvoltage'
     }]
 
 plant = [{
     'type':'plant',
-    'last_update': '10s',
-    'name': '',
-    'soil_moisture': 10
+    'last_update': '11s',
+    'name': 'Ogorki',
+    'soil_moisture': 11
     },
     {
     'type':'plant',
-    'last_update': '10s',
-    'name': '',
-    'soil_moisture': 10
+    'last_update': '12s',
+    'name': 'Roszponka',
+    'soil_moisture': 12
     },
     {
     'type':'plant',
-    'last_update': '10s',
-    'name': '',
-    'soil_moisture': 10
+    'last_update': '13s',
+    'name': 'Rzodkiewka',
+    'soil_moisture': 13
     },
     {
     'type':'plant',
-    'last_update': None,
-    'name': '',
-    'soil_moisture': 0
+    'last_update': '14s',
+    'name': 'Mieta',
+    'soil_moisture': 14
     },
     {
     'type':'plant',
-    'last_update': None,
-    'name': '',
-    'soil_moisture': 0
+    'last_update': '15s',
+    'name': 'Szczypior',
+    'soil_moisture': 15
     },
     {
     'type':'plant',
-    'last_update': None,
-    'name': '',
-    'soil_moisture': 0
+    'last_update': '16s',
+    'name': 'Salata',
+    'soil_moisture': 16
     },
     {
     'type':'plant',
-    'last_update': None,
-    'name': '',
-    'soil_moisture': 0
+    'last_update': '17s',
+    'name': 'Koperek',
+    'soil_moisture': 17
     },
     {
     'type':'plant',
-    'last_update': None,
-    'name': '',
-    'soil_moisture': 0
+    'last_update': '18s',
+    'name': 'Pomidor',
+    'soil_moisture': 18
     },
     {
     'type':'plant',
-    'last_update': None,
-    'name': '',
-    'soil_moisture': 0
+    'last_update': '19s',
+    'name': 'Papryka',
+    'soil_moisture': 19
     }]
+
+notification = []
+
+notification1 = {'type': 'Warning', 'timestamp': '202006301838', 'text': 'Just a simple warning'}
+notification2 = {'type': 'Success', 'timestamp': '202006301839', 'text': 'Just a simple success'}
+notification3 = {'type': 'Danger', 'timestamp': '202006301840', 'text': 'Just a simple danger'}
+notification4 = {'type': 'Info', 'timestamp': '202006301841', 'text': 'Just a simple info'}
+notification.append(notification1)
+notification.append(notification2)
+notification.append(notification3)
+notification.append(notification4)
 
 
 
 def new_client(client, server):
-    msg = json.dumps({'sectors': sector, 'plants': plant, 'watertanks': watertank, 'power': battery})
+    msg = json.dumps({'sectors': sector, 'plants': plant, 'watertanks': watertank, 'power': battery, 'notifications': notification})
     server.send_message_to_all(msg)
     print("new client: ", client['address']," joined")
 
 def new_msg(client, server, message):
-    msg = json.dumps({'sectors': sector, 'plants': plant, 'watertanks': watertank, 'power': battery})
+    msg = json.dumps({'sectors': sector, 'plants': plant, 'watertanks': watertank, 'power': battery, 'notifications': notification})
     server.send_message_to_all(msg)
+    #clear notification after sending
     print("new msg sent")
 
 def client_left(client, server):
